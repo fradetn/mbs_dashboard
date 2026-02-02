a0a5cd4f097cfc08bdde78add59e73d416457d92
Nael <nael.fradet@gmail.com>
2026-02-02 11:34:23 +0100

    dashboard fini

--- my_dashboard.py
+++ my_dashboard.py
@@ -1,25 +1,76 @@
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
+import requests
+from plotly.subplots import make_subplots
+from bs4 import BeautifulSoup
+from collections import Counter
+
 
 st.set_page_config(
-    page_title="Dashboard eSIM - MyBestSim",
-    page_icon="📊",
-    layout="wide"
+	page_title="Dashboard eSIM - MyBestSim",
+	page_icon="📟",
+	layout="wide"
 )
 
-CSV_BASE_URL = "http://d4ow8gwkc800w4o44c8oo8ck.31.97.154.190.sslip.io/csv/"
 
-PROVIDERS  = ["Airalo", "Esim4Travel", "ezyEsim", "Instabridge", "redtea", "yesim", "amigo", "esimo", "ezyesim", "maya", "saily", "breeze", "eSIMX", "gigsky", "microEsim", "Ubigi"]
-PROVIDERSPLANS  = ["AiraloPlans", "Esim4TravelPlans", "ezyEsimPlans", "InstabridgePlans", "redteaPlans", "yesimPlans", "amigoPlans", "esimoPlans", "ezyEsimPlans", "mayaPlans", "sailyPlans", "breezePlans", "eSIMXPlans", "gigskyPlans", "microEsimPlans", "UbigiPlans"]
+CSV_BASE_URL = "http://d4ow8gwkc800w4o44c8oo8ck.31.97.154.190.sslip.io"
 
-# ============ FONCTIONS D'EXTRACTION DES DONNEES ============
+@st.cache_data(ttl=3600)
+def list_provider_files(base_url):
+	"""
+	Liste dynamiquement tous les fichiers de fournisseurs sur le serveur.
+	"""
+	try:
+		response = requests.get(base_url + "/csv/", timeout=10)
+		response.raise_for_status()
+		
+		soup = BeautifulSoup(response.text, "html.parser")
+		
+		
+		providers = []
+		
+		# Méthode 1 : Chercher tous les <a> tags
+		for link in soup.find_all("a"):
+			href = link.get("href")
+			
+			# Filtrer : doit être un lien, pas "../" ou "/", doit contenir du texte
+			if href and href not in ["../", "/", "", "/csv"]:
+				
+				new_response = requests.get(base_url + href, timeout=10)
+				new_response.raise_for_status()
+
+				new_soup = BeautifulSoup(new_response.text, "html.parser")
+				# st.write(new_soup.find_all("a"))
+				for l in new_soup.find_all("a"):
+					new_href = l.get("href")
+					if new_href and new_href.__contains__("Plans.csv"):
+						clean_name = new_href.rstrip("/")
+						providers.append(clean_name)
+		
+		# Dédupliquer et trier
+		providers = sorted(list(set(providers)))
+		
+		return providers
+    
+	except Exception as e:
+		st.error(f"Erreur lors du listing des fournisseurs : {str(e)}")
+	return []
+
+
+PROVIDERS  = list_provider_files(CSV_BASE_URL)
+
+# ==================================== FONCTIONS D'EXTRACTION DES DONNEES ====================================
 @st.cache_data(ttl=3600)
 def load_data_from_csv():
+	"""
+	Lis chaque fichiers depuis son lien pour extraire son contenu
+	dans un dataFrane.
+	"""
 	all_data = []
-	for index in range(0, 16):
+	for index in range(0, len(PROVIDERS)):
 		try:
-			csv_url = CSV_BASE_URL + PROVIDERS[index] + "/" + PROVIDERSPLANS[index] + ".csv"
+			csv_url = CSV_BASE_URL + PROVIDERS[index]
 			df = pd.read_csv(csv_url)
 
 			all_data.append(df)
@@ -35,67 +86,27 @@ def load_data_from_csv():
 		return pd.DataFrame()
 
 
-all_products = load_data_from_csv()
-st.sidebar.title("🔍 Filtres")
+@st.cache_data(ttl=3600)
+def sanitize_data_type(dataframe):
+    """
+    Uniformise les types des colonnes importantes.
+    """
+    df = dataframe.copy()  # Toujours travailler sur une copie
+    
+    # Nettoyer PRIX/GB(EUR)
+    df['PRIX/GB(EUR)'] = df['PRIX/GB(EUR)'].apply(
+        lambda x: float(str(x).replace(',', '.')) if pd.notna(x) else None
+    )
+    
+    return df
+
+# ==================================== Preparation des donnees ====================================
+# all_products = load_data_from_csv()
+
+all_products =  sanitize_data_type(load_data_from_csv())
+
 
 nb_produits = len(all_products)
-
-# ============ DEBUG : Inspecter les données ============
-# st.write("### 🔍 Debug : Structure des données")
-# st.write("**Colonnes disponibles :**", all_products.columns.tolist())
-# st.write("**Premières lignes :**")
-# st.dataframe(all_products.head(10))
-
-# # Voir les types de données
-# st.write("**Types de colonnes :**")
-# st.write(all_products.dtypes)
-
-# ============ Dashboard : Creation des graphiques ============
-
-# Compter les produits et les produits > 100€
-nb_produits_100_plus = len(all_products[all_products['PRIX'] > 100])
-
-# products_info : le graphique pour les produits > 100€
-products_info = go.Figure(data=[go.Pie(labels=['≤ 100€', '> 100€'],
-	values=[nb_produits - nb_produits_100_plus, nb_produits_100_plus])])
-
-products_info.update_traces(hoverinfo='label+percent', 
- 	hole=0.6,
-	textinfo='value',
-	textfont_size=19,
-	marker=dict(colors=['#8b5cf6', '#c4b5fd'], line=dict(color='white', width=2)),
-	hovertemplate='<b>%{label}</b><br>%{value} produits<br>%{percent}<extra></extra>'
-	)
-
-products_info.update_layout(
-    title={
-        'text': "📊 Répartition des Produits par Prix",
-        'x': 0.5,
-        'xanchor': 'center'
-    },
-	legend=dict(
-		title=dict(
-			text= "Prix des produits",
-			font=dict(
-				size=17,
-        	),
-        ),
-		font=dict(
-            size=15,
-        )
-	),
-	title_font=dict(size=25),
-    showlegend=True,
-    height=400,
-    margin=dict(t=80, b=20, l=20, r=20)
-)
-
-products_info.add_annotation(
-    text=f"<b>{nb_produits:,}</b><br><span style='font-size:14px'>produits total</span>",
-    x=0.5, y=0.5,
-    font=dict(size=24),
-    showarrow=False
-)
 
 price_range_df = (
     all_products
@@ -106,6 +117,93 @@ price_range_df = (
     )
     .reset_index()
 )
+
+products_count_df = (
+    all_products
+    .groupby("NOM ENTREPRISE")
+    .size()
+    .reset_index(name="nb_produits")
+)
+
+provider_avg_df = (
+    all_products
+    .groupby("NOM ENTREPRISE")
+    .agg(
+        avg_price=("PRIX", "mean"),
+        avg_data_go=("DONNEES (GO)", "mean")
+    )
+    .reset_index()
+)
+
+price_per_data_df = (
+    all_products
+    .groupby("NOM ENTREPRISE")
+    .agg(
+        avg_price_per_data=("PRIX/GB(EUR)", "mean"),
+    )
+    .reset_index()
+)
+price_per_data_df = price_per_data_df.sort_values('avg_price_per_data')
+
+# Extraire tous les pays
+all_countries = []
+for countries_str in all_products['COUVERTURE'].dropna():
+    countries = [c.strip() for c in str(countries_str).split(',')]
+    all_countries.extend(countries)
+
+# Compter les occurrences
+country_counts = Counter(all_countries)
+top_10_countries = pd.DataFrame(
+    country_counts.most_common(10),
+    columns=['Pays', 'Nombre de produits']
+)
+
+# ==================================== Dashboard : Creation des graphiques ====================================
+
+# Compter les produits et les produits > 100€
+nb_products_100_more = len(all_products[all_products['PRIX'] > 100])
+
+# products_info : le graphique pour les produits > 100€
+products_info_fig = go.Figure(data=[go.Pie(
+    labels=['≤ 100€', '> 100€'],
+    values=[nb_produits - nb_products_100_more, nb_products_100_more],
+    hole=0.6,
+    textinfo='value',
+    textfont_size=19,
+    marker=dict(
+        colors=['#8b5cf6', '#c4b5fd'],
+        line=dict(color='white', width=2)
+    ),
+    hovertemplate='<b>%{label}</b><br>%{value} produits<br>%{percent}<extra></extra>'
+)])
+
+# Layout responsive
+products_info_fig.update_layout(
+    legend=dict(
+        title=dict(
+            text="Prix des produits",
+            font=dict(size=15),
+        ),
+        font=dict(size=14),
+        orientation="v",
+		x=1,
+		y=0.5,
+		yanchor="bottom",
+		xanchor="left",
+    ),
+    showlegend=True,
+  	height=250,
+    margin=dict(t=20, b=20, l=0, r=0),  # Marges proportionnelles
+)
+
+# Annotation centrale
+products_info_fig.add_annotation(
+    text=f"<b>{nb_produits:,}</b><br><span style='font-size:0.8em'>produits total</span>",
+    x=0.5, y=0.5,
+    font=dict(size=14),
+    showarrow=False
+)
+
 
 global_min_price = price_range_df["min_price"].min()
 global_max_price = price_range_df["max_price"].max()
@@ -137,18 +235,245 @@ styled_df = (
     })
 )
 
-# ============ Dashboard : Affichage des graphiques  ============
 
-col1, col2 = st.columns(2)
-with col1:
-	with st.container(border=True):
-		st.plotly_chart(products_info, use_container_width=True)
-with col2:
-	with st.container(border=True):
-		st.subheader("📊 Fourchette de prix par fournisseur")
-		st.caption("Prix minimum et maximum observés dans le catalogue")
+
+
+prod_per_providers_fig = go.Figure()
+
+prod_per_providers_fig.add_trace(
+    go.Bar(
+        x=products_count_df["NOM ENTREPRISE"],
+        y=products_count_df["nb_produits"],
+        text=products_count_df["nb_produits"],
+        textposition="outside",
+        hovertemplate=(
+            "Fournisseur: %{x}<br>"
+            "Nombre d'offres: %{y}<extra></extra>"
+        ),
+        name="Nombre d'offres"
+    )
+)
+
+prod_per_providers_fig.update_layout(
+    xaxis_title="Fournisseur",
+    yaxis_title="Nombre d'offres",
+    height=390,
+    margin=dict(t=0, b=0, l=0, r=0)
+)
+
+global_avg_price = all_products["PRIX"].mean()
+global_avg_data = all_products["DONNEES (GO)"].mean()
+
+avg_price_data_ = go.Figure()
+
+# 🔵 Prix moyen (axe gauche)
+avg_price_data_.add_trace(
+    go.Bar(
+        x=provider_avg_df["NOM ENTREPRISE"],
+        y=provider_avg_df["avg_price"],
+        name="Prix moyen (€)",
+        yaxis="y1",
+        hovertemplate="Prix moyen: %{y:.2f} €<extra></extra>",
+    )
+)
+
+# 🟢 Data moyenne (axe droit)
+avg_price_data_.add_trace(
+    go.Bar(
+        x=provider_avg_df["NOM ENTREPRISE"],
+        y=provider_avg_df["avg_data_go"],
+        name="Data moyenne (Go)",
+        yaxis="y2",
+        hovertemplate="Data moyenne: %{y:.2f} Go<extra></extra>",
+    )
+)
+
+# ➖ Prix moyen global
+
+avg_price_data_ = make_subplots(
+    rows=1,
+    cols=2,
+    subplot_titles=(
+        "Prix moyen (€) par fournisseur",
+        "Data moyenne (Go) par fournisseur"
+    )
+)
+
+# 🔵 Prix moyen
+avg_price_data_.add_trace(
+    go.Scatter(
+        x=provider_avg_df["avg_price"],
+        y=provider_avg_df["NOM ENTREPRISE"],
+        mode="markers",
+        name="Prix moyen",
+        marker=dict(size=10),
+        hovertemplate="%{y}<br>Prix moyen: %{x:.2f} €<extra></extra>"
+    ),
+    row=1,
+    col=1
+)
+
+avg_price_data_.add_vline(
+    x=global_avg_price,
+    line_dash="dash",
+    line_color="red",
+    row=1,
+    col=1
+)
+
+avg_price_data_.add_annotation(
+    x=global_avg_price,
+    y=-0.5,  # sous le dernier fournisseur
+    text=f"Moyenne globale: {global_avg_price:.2f} €",
+    showarrow=False,
+    xanchor="center",
+    yanchor="top",
+    row=1,
+    col=1
+)
+
+# 🟢 Data moyenne
+avg_price_data_.add_trace(
+    go.Scatter(
+        x=provider_avg_df["avg_data_go"],
+        y=provider_avg_df["NOM ENTREPRISE"],
+        mode="markers",
+        name="Data moyenne",
+        marker=dict(size=10),
+        hovertemplate="%{y}<br>Data moyenne: %{x:.2f} Go<extra></extra>"
+    ),
+    row=1,
+    col=2
+)
+
+avg_price_data_.add_vline(
+    x=global_avg_data,
+    line_dash="dash",
+    line_color="red",
+    row=1,
+    col=2
+)
+
+avg_price_data_.update_layout(
+    height=550,
+    title="➡ Moyennes par fournisseur vs moyennes globales",
+    showlegend=False
+)
+
+avg_price_data_.add_annotation(
+    x=global_avg_data,
+    y=-0.5,
+    text=f"Moyenne globale: {global_avg_data:.2f} Go",
+    showarrow=False,
+    xanchor="center",
+    yanchor="top",
+    row=1,
+    col=2
+)
+
+
+
+provider_price_per_data_fig = go.Figure()
+
+provider_price_per_data_fig.add_trace(
+    go.Bar(
+        x=price_per_data_df["NOM ENTREPRISE"], 
+        y=price_per_data_df["avg_price_per_data"],
+        text=price_per_data_df["avg_price_per_data"].round(2),  # Arrondir à 2 décimales
+        texttemplate='%{text:.2f}€/GB',  # Format avec €/GB
+        textposition="outside",
+        hovertemplate=(
+            "<b>%{x}</b><br>"
+            "Prix moyen: <b>%{y:.2f}€/GB</b>"
+            "<extra></extra>"
+        ),
+        name="Prix moyen du GB",
+        marker=dict(
+            color=price_per_data_df["avg_price_per_data"],
+            colorscale='RdYlGn_r',  # Rouge (cher) → Vert (pas cher)
+            showscale=True,
+            colorbar=dict(title="€/GB")
+        )
+    )
+)
+
+# Layout
+provider_price_per_data_fig.update_layout(
+    title={
+        'text': "💰 Prix Moyen du GB par Fournisseur",
+        'x': 0.5,
+        'xanchor': 'center'
+    },
+    xaxis_title="Fournisseur",
+    yaxis_title="Prix/GB (€)",
+    height=500,
+    showlegend=False,
+    hovermode='x unified'
+)
+
+
+top_10_countries = top_10_countries.sort_values('Nombre de produits', ascending=True)
+
+# Créer le graphique avec graph_objects
+top_10_countries_fig = go.Figure()
+
+top_10_countries_fig.add_trace(go.Bar(
+    x=top_10_countries['Nombre de produits'],
+    y=top_10_countries['Pays'],
+    orientation='h',
+    marker=dict(
+        color=top_10_countries['Nombre de produits'],
+        colorscale='Blues',
+        showscale=True,
+        colorbar=dict(
+            title="Produits",
+            thickness=10,
+            len=0.5
+        )
+    ),
+    text=top_10_countries['Nombre de produits'],
+    texttemplate='%{text}',
+    textposition='inside',
+    hovertemplate='<b>%{y}</b><br>%{x} produits<extra></extra>'
+))
+
+# Layout
+top_10_countries_fig.update_layout(
+    xaxis_title="Nombre de produits",
+    yaxis_title="",
+    height=510,
+    showlegend=False,
+    margin=dict(t=0, b=0, l=0, r=0)
+)
+
+# ==================================== Dashboard : Affichage des graphiques  ====================================
+
+# ============ TITRE ============
+st.title("Dashboard eSIM - MyBestSim", text_alignment="center")
+st.space("medium")
+
+col_large, col_small = st.columns([2, 1], gap="small")
+with col_large:
+	with st.container(border=True, height="content"):
+		st.markdown("**📊 Produits par Fournisseur**")
+		st.plotly_chart(prod_per_providers_fig, use_container_width=True)
+	with st.container(border=True, height="content", width="stretch"):
+		st.plotly_chart(provider_price_per_data_fig, use_container_width=True)		
+with col_small:
+	with st.container(border=True, height="content"):
+		st.markdown("**💎 Nombre de Produits**")
+		st.plotly_chart(products_info_fig, use_container_width=True)
+	with st.container(border=True, height="content", width="stretch"):
+		st.markdown("**💵 Fourchette de Prix**")
 		st.dataframe(styled_df, 
-		height=400,
+		height=600,
 		use_container_width=True)
 
-
+col_small, col_large = st.columns([1, 2], gap="small")
+with col_large:
+	with st.container(border=True, height="content"):
+		st.plotly_chart(avg_price_data_, use_container_width=True)
+with col_small:
+	with st.container(border=True, height="content"):
+		st.markdown("**🌍 Top 10 Pays les Plus Couverts**")
+		st.plotly_chart(top_10_countries_fig, use_container_width=True)
